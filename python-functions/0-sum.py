ba05407a0a18b92736ecd65d5acbd3c7bf7e1af6
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-20 18:59:22 +0300

    added a comment

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,2 +1,9 @@
 def add(a, b):
     return a + b
+
+# Write a function that adds two integers and returns the result.
+
+# Prototype: def add(a, b):
+# Returns the value of a + b
+# You are not allowed to import any module
+# You don’t need to understand __import__
