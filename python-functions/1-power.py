c180a4abf6de458dec61e14482f08b6d7cbd382e
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-20 18:49:43 +0300

    a^b; a function that computes 'a' to the power of 'b'

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,12 @@
 def pow(a, b):
-    result = 1.0
-    for _ in range(abs(b)):
-        result *= a
+    result = 1
+
     if b < 0:
-        result = 1 / result
+        a = 1 / a
+        b = -b
+
+    for _ in range(b):
+        result *= a
+
     return result
 
