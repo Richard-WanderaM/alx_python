6ebabd2187aee9429497142b863631f10280c41f
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-20 19:05:58 +0300

    added the question

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,3 +1,10 @@
 def reverse_string(string):
     return string[::-1]
 
+
+# Write a Python function called reverse_string that takes a string as input and returns the reverse of that string.
+
+# Prototype: def reverse_string(string)
+# Returns the reversed string.
+# You are not allowed to import any module.
+# You don’t need to understand __import__
