e79e737827bedb8ee8ba2b8311a0c9fc88a891be
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-20 14:37:47 +0300

    added a comment

--- 2-positive_or_negative.py
+++ 2-positive_or_negative.py
@@ -4,6 +4,9 @@ import random
 
 number = random.randint(-10, 10)
 
+# Check if the number is positive, negative,
+# or zero and print the appropriate message
+
 if number > 0:
     print(number, "is positive")
 elif number ==0:
