03df0c841b87e026283cda6520ac094df79c8cc6
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-24 13:30:57 +0300

    corrected my code

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -15,9 +15,7 @@ def add(a, b):
 
 if __name__ == "__main__":
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
-
-
+    print("{} + {} = {}".format(a, b, result))
 
 # Write a program that imports the function def add(a, b): 
 # from the file add_0.py and prints the result of the addition 1 + 2 = 3
