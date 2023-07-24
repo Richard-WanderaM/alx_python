708c51771ab7f92c3afceca6b403e7648b35cd23
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-24 14:03:43 +0300

    added a comment

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,2 +1,7 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
+
+# Write a function that raises a name exception with a message.
+
+# Prototype: def raise_exception_msg(message=""):
+# You are not allowed to import any module
