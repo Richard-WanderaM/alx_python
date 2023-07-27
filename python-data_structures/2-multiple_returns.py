c13372a485a15a2704de585673e2ad2eda681288
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-27 21:06:44 +0300

    comments

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -4,10 +4,13 @@ def multiple_returns(sentence):
     else:
         return len(sentence), sentence[0]
 
-# if not sentence:: This checks if the sentence is empty. An empty string evaluates to False, so this condition is true when sentence is empty.
+# if not sentence:: This checks if the sentence is empty. 
+# An empty string evaluates to False, so this condition is true when sentence is empty.
 
-# return 0, None: If the sentence is empty, the function returns a tuple with the length 0 and the first character as None.
+# return 0, None: If the sentence is empty,
+# the function returns a tuple with the length 0 and the first character as None.
 
 # else:: If the sentence is not empty, the function proceeds to the next part.
 
-# return len(sentence), sentence[0]: If the sentence is not empty, the function returns a tuple with the length of the sentence and the first character of the sentence.
+# return len(sentence), sentence[0]: If the sentence is not empty, 
+# the function returns a tuple with the length of the sentence and the first character of the sentence.
