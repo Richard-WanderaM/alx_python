6deac63e05d46412499e55cbcd35b3bdb08a9c4c
Richard-WanderaM <richardwandera2023@gmail.com>
2023-07-28 17:45:08 +0300

    a function that returns a set of common elements in two sets

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,15 @@
+ # Calculate the intersection of the two sets and return the result
+
+def common_elements(set_1, set_2):
+
+    return set_1.intersection(set_2)
+
+# We create the common_elements function by using the intersection operation between the two sets.
+# This operation returns a new set that contains elements that are common to both sets.
+    
+
+#QUESTION
+# Write a function that returns a set of common elements in two sets.
+
+# Prototype: def common_elements(set_1, set_2):
+# You are not allowed to import any module
