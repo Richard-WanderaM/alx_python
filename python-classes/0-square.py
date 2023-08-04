5cc0ad1ef250bcaeb9081aa85774516e3640b406
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-04 20:41:03 +0300

    documented my code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,32 @@
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size (side length) of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size (side length) of the square.
+        """
         self.__size = size
 
+# Test code
 if __name__ == "__main__":
     my_square = Square(3)
     print(type(my_square))
     print(my_square.__dict__)
 
     try:
-        print(my_square.size)  # Attempting to access private attribute directly raises an error
+        print(my_square.size)  # Will raise an AttributeError
     except Exception as e:
         print(e)
 
     try:
-        print(my_square.__size)  # Attempting to access private attribute with name mangling raises an error
+        print(my_square.__size)  # Will raise an AttributeError
     except Exception as e:
         print(e)
