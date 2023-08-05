d963f384a9507c1bf5b4ee32460344d5023604cd
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-05 17:36:09 +0300

    added a shebang

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,13 @@
-class Square:
-    """
+#!/usr/bin/python3
+
+"""
     This class represents a square.
 
     Attributes:
         __size (int): The size (side length) of the square.
     """
 
+class Square:
     def __init__(self, size):
         self.__size = size
 
