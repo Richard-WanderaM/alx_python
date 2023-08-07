eedf7e9d6731d9690a6463c8044b760842b0db24
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-07 23:37:34 +0300

    Display #0

---
+++ python-almost_a_circle/models/rectangle4.py
@@ -0,0 +1,104 @@
+# Question
+
+# Update the class Rectangle by adding the public method def display(self):
+# that prints in stdout the Rectangle instance with the character 
+# - you don’t need to handle x and y here.
+
+#!/usr/bin/python3
+""" Rectangle module """
+from models.base import Base
+
+class Rectangle(Base):
+    """ Rectangle class, inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ Calculate and return the area of the rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """ Display the rectangle using # characters """
+        for _ in range(self.__height):
+            print("#" * self.__width)
+
+if __name__ == "__main__":
+    """ Test cases """
+    r1 = Rectangle(4, 6)
+    r1.display()
+
+    print("---")
+
+    r2 = Rectangle(2, 2)
+    r2.display()
+
+
+# Explanation to my solution
+
+# The display method is added to the Rectangle class.
+#  It uses a loop to print # characters for the specified height of the rectangle.
+
+# The provided test cases in the 4-main.py script create instances of the Rectangle class,
+#  and call the display method to print the rectangles using # characters
