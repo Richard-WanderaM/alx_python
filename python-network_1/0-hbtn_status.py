6528fa9ebf4a68267f4f9e8c85bf6f2f538f0393
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-08 14:55:31 +0300

    A Python script that fetches a link

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@ Module to fetch and display the status of a website.
 
 import requests
 
-def fetch_hbtn_status():
+def fetch_custom_status():
     """
-    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
+    Fetches and displays the status from http://0.0.0.0:5050/status.
     """
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'http://0.0.0.0:5050/status'
     response = requests.get(url)
     content_type = type(response.text)
     content = response.text
@@ -19,6 +19,6 @@ def fetch_hbtn_status():
     print(f"\t- content: {content}")
 
 if __name__ == "__main__":
-    fetch_hbtn_status()
+    fetch_custom_status()
 
 
