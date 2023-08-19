5ec71158e7967d66b7984d13be540fee0aee3c22
Richard Miruka Onsare <138195577+Richard-WanderaM@users.noreply.github.com>
2023-08-19 10:26:01 +0300

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,40 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)  # Attempting to access private attribute directly raises an error
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)  # Attempting to access private attribute with name mangling raises an error
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")  # Attempting to initialize with a non-integer value raises an error
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)  # Attempting to initialize with a negative value raises an error
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
