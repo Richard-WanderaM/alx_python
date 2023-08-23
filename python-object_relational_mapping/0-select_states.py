b6ee81842bb6262ca5518decc46854cd1949edd3
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-24 00:39:54 +0300

    script that lists all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
@@ -14,4 +13,4 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-    
+
