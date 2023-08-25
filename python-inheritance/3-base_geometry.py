d35280a6357db11ec3d12710af4ba90df6780c6e
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-25 17:26:55 +0300

    Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module containing BaseGeometry class """
+
+class BaseGeometry:
+    """ Empty class for demonstration """
+
+if __name__ == "__main__":
+    # Add any test code here
+    pass
