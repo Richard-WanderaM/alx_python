9da8592cb798d26215b68dca35f9d680b4d5675c
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-25 16:18:35 +0300

    a class Square that defines a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class that represents a square by its size.
+"""
+
 class Square:
+    """
+    This class defines a square by its size.
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a square with an optional size.
+        """
         self.size = size
 
     @property
     def size(self):
+        """
+        Getter method to retrieve the size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter method to set the size of the square.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -15,17 +34,21 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Calculates and returns the area of the square.
+        """
         return self.__size ** 2
 
+# Testing the Square class
 if __name__ == "__main__":
-    my_square = Square(89)
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-    my_square.size = 3
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
     try:
-        my_square.size = "5 feet"  # Attempting to set size with a non-integer value raises an error
-        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+        my_square = Square(5)
+        print(my_square.__dict__)
+        print("Area:", my_square.area())
+
+        my_square.size = 10
+        print("New size:", my_square.size)
+        
+        invalid_square = Square("not_an_integer")
     except Exception as e:
         print(e)
