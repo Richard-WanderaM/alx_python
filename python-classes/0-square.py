866ff4cc9b627f09925eb9d4b4239762330e4f4a
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-25 15:55:54 +0300

    a class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,4 +5,19 @@
 
 class Square:
     """Represent a square."""
-    pass
+    def __init__(self, size):
+        self.__size = size
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
