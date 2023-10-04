f74d6a481fd4be510586378b57f5952fcea737c5
Richard-WanderaM <richardwandera2023@gmail.com>
2023-10-04 15:59:40 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,25 +2,20 @@ import requests
 import sys
 
 def get_employee_info(employee_id):
-    # Define the API endpoints
     base_url = "https://jsonplaceholder.typicode.com"
     user_endpoint = f"{base_url}/users/{employee_id}"
     todos_endpoint = f"{base_url}/users/{employee_id}/todos"
 
-    # Make API requests to get employee info and TODO list
     user_response = requests.get(user_endpoint)
     todos_response = requests.get(todos_endpoint)
 
-    # Check if the requests were successful
     if user_response.status_code != 200 or todos_response.status_code != 200:
         print("Failed to retrieve data from the API.")
-        return None, None
+        return None, None, None, None
 
-    # Parse JSON responses
     user_data = user_response.json()
     todos_data = todos_response.json()
 
-    # Extract relevant information
     employee_name = user_data.get("name", "Unknown")
     total_tasks = len(todos_data)
     completed_tasks = sum(1 for todo in todos_data if todo["completed"])
