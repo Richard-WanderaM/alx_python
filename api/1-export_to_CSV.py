4b82fed6557adb72a718f594d6f603febfba124f
Richard-WanderaM <richardwandera2023@gmail.com>
2023-10-10 18:51:25 +0300

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,55 +1,29 @@
-import requests
-import sys
 import csv
-
-def get_employee_info(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_endpoint = f"{base_url}/users/{employee_id}"
-    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
-
-    user_response = requests.get(user_endpoint)
-    todos_response = requests.get(todos_endpoint)
-
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Failed to retrieve data from the API.")
-        return None, None, None, None
-
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-
-    employee_name = user_data.get("name", "Unknown")
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo["completed"])
-    completed_task_titles = [todo["title"] for todo in todos_data if todo["completed"]]
-
-    return employee_name, total_tasks, completed_tasks, completed_task_titles
-
-def display_todo_progress(employee_name, total_tasks, completed_tasks, completed_task_titles):
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for task_title in completed_task_titles:
-        print(f"\t{task_title}")
-
-def export_to_csv(employee_id, employee_name, completed_task_data):
-    filename = f"{employee_id}.csv"
-    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(header)
-
-        for task in completed_task_data:
-            writer.writerow([employee_id, employee_name, "True", task])
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(argv) < 2:
         print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+        exit(1)
 
-    employee_id = int(sys.argv[1])
-    employee_name, total_tasks, completed_tasks, completed_task_titles = get_employee_info(employee_id)
+    employee_id = argv[1]
 
-    if employee_name is not None:
-        display_todo_progress(employee_name, total_tasks, completed_tasks, completed_task_titles)
+    # Fetch employee details
+    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_data = employee_response.json()
+    employee_name = employee_data.get('name')
 
-        # Export data to CSV
-        export_to_csv(employee_id, employee_name, completed_task_titles)
+    # Fetch employee TODO list
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    todos_data = todos_response.json()
+
+    # Create CSV file
+    with open(f"{employee_id}.csv", mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for task in todos_data:
+            writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
+
+    print(f"Data exported to {employee_id}.csv")
