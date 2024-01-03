13a3f36db90818c0e338c36bd8c76ce4ffda40a1
Richard Miruka Onsare <138195577+Richard-WanderaM@users.noreply.github.com>
2024-01-03 22:03:17 +0300

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,47 +1,19 @@
-"""
-This script retrieves employee TODO list progress and exports it to a JSON file.
-
-Usage: python3 2-export_to_JSON.py <employee_id>
-
-Arguments:
-    employee_id (int): The employee ID to retrieve TODO list progress for.
-
-Example:
-    python3 2-export_to_JSON.py 2
-"""
-
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
-from sys import argv
 import requests
+import sys
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-        exit(1)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = argv[1]
-
-    # Fetch employee details
-    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    employee_data = employee_response.json()
-    employee_name = employee_data.get('name')
-
-    # Fetch employee TODO list
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    todos_data = todos_response.json()
-
-    # Filter tasks owned by this employee
-    employee_tasks = [{"task": task['title'], "completed": task['completed'], "username": employee_name} for task in todos_data]
-
-    # Create a dictionary for JSON export
-    export_data = {employee_id: employee_tasks}
-
-    # Export to JSON file
-    with open(f"{employee_id}.json", 'w') as json_file:
-        json.dump(export_data, json_file)
-
-    # Print correct user ID and its length
-    print(f"Correct USER_ID: {employee_id}, Length: {len(employee_id)}")
-
-
-
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
